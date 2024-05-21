9cdc9843724934af5b884aa44237441542c560e9
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-05-21 19:30:26 +0200

    hell dunkul mode in bearbeitung

---
+++ JavaScript/mode.js
@@ -0,0 +1,11 @@
+/* let modeicon = document.getElementById("modeicon")
+
+modeicon.onclick = function () {
+  document.body.classList.toggle("dark-theme")
+  if (document.body.classList.contains("dark-theme")) {
+    modeicon.src = "Images/sun.png"
+  } else {
+    modeicon.src = "Images/moon.png"
+  }
+}
+ */
